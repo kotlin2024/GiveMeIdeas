2c27329b65bd65014034edce9a0671eeabaf82dc
PHJUNITY <45454119+PHJUNITY@users.noreply.github.com>
2024-11-13 15:50:05 +0900

    Initial commit

---
+++ src/main/kotlin/hjp/givemeideas/GiveMeIdeasApplication.kt
@@ -0,0 +1,11 @@
+package hjp.givemeideas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GiveMeIdeasApplication
+
+fun main(args: Array<String>) {
+	runApplication<GiveMeIdeasApplication>(*args)
+}
---
+++ src/test/kotlin/hjp/givemeideas/GiveMeIdeasApplicationTests.kt
@@ -0,0 +1,13 @@
+package hjp.givemeideas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GiveMeIdeasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
